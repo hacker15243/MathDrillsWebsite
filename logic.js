61348809f3916138b55e3d77090cdd2cd48630ab
hacker15243 <goyal.aadhya123@gmail.com>
2025-01-26 09:10:02 -0800

    update

--- logic.js
+++ logic.js
@@ -5,9 +5,13 @@ var answer;
 var totalQuestions = 0;
 var score = 0;
 
+var questionContainer;
+
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("The DOM is fully loaded!");
+    questionContainer = document.getElementById("question")
+    console.log(questionContainer);
     onStart();
 });
 
@@ -35,7 +39,10 @@ function handleSubmit(event){
     updateScores();
 
     //wait and then call generate question again
-    setTimeout(generateQuestion, 1000);
+    // setTimeout(generateQuestion, 1000);
+
+    //don't wait, generate quetsion
+    generateQuestion();
 
 }
 
@@ -44,15 +51,24 @@ function checkAnswer(){
 }
 
 function generateQuestion(){
-    num1 = Math.round(Math.random() * 100);
-    num2 = Math.round(Math.random() * 100);
-    answer = num1 + num2;
-    document.getElementById("question").innerHTML = num1 + " + " + num2 + " = ";
+    num1 = Math.round(Math.random() * 12);
+    num2 = Math.round(Math.random() * 12);
+    answer = num1 * num2;
+
+    //create a new line of text
+    const temp = document.createElement("span");
+    temp.textContent = num1 + " x " + num2 + " = ";
+
+    //Add a line break and then add the new line of text created above
+    questionContainer.appendChild(document.createElement("br"));
+    questionContainer.appendChild(temp);
+    // document.getElementById("question").innerHTML = num1 + " + " + num2 + " = ";
 }
 
 function updateScores(){
     document.getElementById("score").innerHTML = "Score: " + score;
     document.getElementById("Accuracy").innerHTML = "Accuracy: " + Math.round((score/totalQuestions) * 100) + " %";
-    document.getElementById("question").innerHTML = num1 + " + " + num2 + " = " + userAnswer;
+    questionContainer.innerHTML += userAnswer;
+    // document.getElementById("question").innerHTML = num1 + " + " + num2 + " = " + userAnswer;
     document.getElementById("totalQuestions").innerHTML = "Total Questions Answered: " + totalQuestions;
 }
