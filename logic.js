86afbaba269bec4d3231829416a126a9bce93946
Aadhya-goyal <2024873@apps.nsd.org>
2025-10-26 16:46:36 -0700

    update

--- logic.js
+++ logic.js
@@ -11,6 +11,8 @@ var sec = 10; //start time
 // var 
 var timerElement;
 
+
+
 //TODO: add something to store data locally
 //  add something so that users can see their high score total
 //  DONE: add a time function
@@ -46,7 +48,6 @@ function handleSubmit(event){
     document.getElementById("answer").value = "";
     
     handleSubmitHelper();
-
 }
 
 function handleSubmitHelper(){
@@ -55,15 +56,23 @@ function handleSubmitHelper(){
 
     // alert(`your answer is: ${answer}`);
     //this part checks the answer and based on that it updates the scores and stuff
-    if(checkAnswer()){
+    if(checkAnswer()){//if correct
         score ++;
         totalQuestions ++;
-    } else {
-        totalQuestions ++;
+        document.getElementById("question"+(totalQuestions-1)).style.color = "green";//change text color to green
+        // document.getElementById();
+        updateScores();
+
+    } else {//if wrong
+        totalQuestions ++;//increase total question count
+        document.getElementById("question"+(totalQuestions-1)).style.color = "red";//change color of text to red
+        updateScores();
     }
 
+    
+
     //this part updates everything now
-    updateScores();
+    
 
     //wait and then call generate question again
     // setTimeout(generateQuestion, 1000);
@@ -77,13 +86,14 @@ function checkAnswer(){
 }
 
 function generateQuestion(){
-    num1 = Math.round(Math.random() * 12);
-    num2 = Math.round(Math.random() * 12);
-    answer = num1 * num2;
+    num1 = Math.round(Math.random() * 100);
+    num2 = Math.round(Math.random() * 100);
+    answer = num1 - num2;
 
     //create a new line of text
     const temp = document.createElement("span");
-    temp.textContent = num1 + " x " + num2 + " = ";
+    temp.id = "question" + totalQuestions;
+    temp.textContent = num1 + " - " + num2 + " = ";
 
     //Add a line break and then add the new line of text created above
     questionContainer.appendChild(document.createElement("br"));
@@ -92,7 +102,7 @@ function generateQuestion(){
         // container.scrollTop = container.scrollHeight;
         (document.getElementsByClassName("scrollable-content")[0]).scrollTop = (document.getElementsByClassName("scrollable-content")[0]).scrollHeight;
         const container = document.getElementsByClassName("scrollable-content")[0];
-console.log(container)
+        console.log(container)
     }, 0);
     console.log(document.getElementsByClassName("scrollable-content")[0])
     console.log(document.getElementsByClassName("scrollable-content")[0].scrollTop);
@@ -106,10 +116,14 @@ console.log(container)
 function updateScores(){
     document.getElementById("score").innerHTML = "Score: " + score;
     document.getElementById("Accuracy").innerHTML = "Accuracy: " + Math.round((score/totalQuestions) * 100) + " %";
+    // if()
     questionContainer.innerHTML += userAnswer;
     // document.getElementById("question").innerHTML = num1 + " + " + num2 + " = " + userAnswer;
     document.getElementById("totalQuestions").innerHTML = "Total Questions Answered: " + totalQuestions;
 }
+
+
+
 
 function formatTime(sec){
     const hours = String(Math.floor(sec / 3600)).padStart(2, "0");
@@ -118,6 +132,8 @@ function formatTime(sec){
   return "Time left: " + seconds + " seconds";
 //   return `${hours}:${minutes}:${seconds}`;
 }
+
+
 
 // Handles the time stuff of everything
 //starts the time and ticks down one second each second and updates the html
